7653dff05b434201253ee63090aef2145de15178
Jan Liebeck <jliebeck@stack-one.tech>
2025-06-12 12:55:42 +0200

    some changes {styles.   } to use actually scss

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,22 @@
-import './styles/GameBoard.module.scss'
+import styles from './styles/GameBoard.module.scss';
 import GameBoard from "./components/GameBoard";
 
 export default function Page() {
     return (
-        <div className="wrapper">
+        <div className={styles.wrapper}>
             <h1>Tic Tac Toe</h1>
-            <div className="score">
-                <div className="scoreX">
+            <div className={styles.score}>
+                <div className={styles.scoreX}>
                     Player X: <span id="scoreXId">0</span>
                 </div>
-                <div className="scoreO">
+                <div className={styles.scoreO}>
                     Player O: <span id="scoreOId">0</span>
                 </div>
             </div>
 
-            <div className="board" id="board">
+            <div className={styles.board} id="board">
                 <GameBoard />
             </div>
-
-            <button className="reset-b">Reset</button>
         </div>
     )
 }
